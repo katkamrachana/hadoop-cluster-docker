6e897229bf44d6dac0430ddc292a9dee79c6046c
katkamrachana <katkam.rachana@gmail.com>
2018-09-04 12:34:14 -0500

    container start cmds moved to separate file

---
+++ start-containers.sh
@@ -0,0 +1,5 @@
+# start containers
+sudo docker exec -it hadoop-master bash
+sudo docker exec -it hadoop-slave1 bash
+sudo docker exec -it hadoop-slave2 bash
+sudo docker exec -it hadoop-slave3 bash
