68a4429be4330f7cc0dfb30be03246db9346eb2f
katkamrachana <katkam.rachana@gmail.com>
2018-09-04 16:11:55 -0500

    wordcount bash file

--- config/run-wordcount.sh
+++ config/run-wordcount.sh
@@ -2,9 +2,11 @@
 
 # test the hadoop cluster by running wordcount
 
-# uncomment following lines:
+# uncomment following line:
 # hadoop fs -mkdir -p input # create input directory on HDFS
-# hdfs dfs -put ./input/* input # put input files to HDFS
+
+# ignore the error message when put is executed more than once
+hdfs dfs -put ./input/* input # put input files to HDFS
 
 # run wordcount 
 hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/sources/hadoop-mapreduce-examples-2.7.2-sources.jar org.apache.hadoop.examples.WordCount input output
